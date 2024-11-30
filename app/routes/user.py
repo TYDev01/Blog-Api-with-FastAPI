f9ddf91faceba7f4467806ef0528b7b94faae4b6
Anthony <anthonychinaemeru84@yahoo.com>
2024-11-30 23:38:23 +0100

    Update: Check password length

--- app/routes/user.py
+++ app/routes/user.py
@@ -11,6 +11,7 @@ router = APIRouter()
 
 @router.post("/register_user", status_code=status.HTTP_200_OK)
 async def register_user(new_user: Registeration, db: Session = Depends(get_db)):
+    # Check if email already exists
     email = new_user.email
     does_email_exists = db.exec(select(Registeration).where(Registeration.email == email)).first()
     if does_email_exists:
@@ -19,6 +20,7 @@ async def register_user(new_user: Registeration, db: Session = Depends(get_db)):
     else:
         print("Email is available")
 
+    # Check if username already exists
     username = new_user.username
     does_username_exists = db.exec(select(Registeration).where(Registeration.username == username)).first()
 
@@ -27,7 +29,11 @@ async def register_user(new_user: Registeration, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Username already taken.")
     else:
         print("Username is available")
+    
+
     password = new_user.password
+    if len(password) < 8:
+        raise HTTPException(status_code=400, detail="password should not be less than 8 characters.")
     hashing_the_password =  bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
     new_user.password = hashing_the_password
     await sendmail(new_user.email, new_user.username)
