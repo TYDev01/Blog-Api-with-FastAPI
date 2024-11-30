ae44215ef0cd6411a6ae352c35613996307e8166
Anthony <anthonychinaemeru84@yahoo.com>
2024-11-30 21:31:33 +0100

    Update: Improved error handlling, little issues with updating time_registered.

--- app/models/models.py
+++ app/models/models.py
@@ -1,10 +1,15 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
-from datetime import datetime
+# from datetime import datetime,timezone
+from pydantic import EmailStr
 
 class Registeration(SQLModel, table=True):
     __tablename__ = "user"
     id: Optional[int] = Field(default=None, primary_key=True)
-    email: str = Field(index=True, unique=True)
+    email: EmailStr = Field(index=True, unique=True)
     username: str = Field(unique=True, index=True)
     password: str
+    # date_registered: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    # class Config:
+    #     from_attributes = True
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,24 +1,37 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, APIRouter, status
 from models.models import Registeration
 from sqlmodel import Session, select
 from utils.database import init_db, get_db
+import bcrypt
 init_db()
 app = FastAPI()
+router = APIRouter()
 
-@app.post("/register_user")
+@router.post("/register_user", status_code=status.HTTP_200_OK)
 async def register_user(new_user: Registeration, db: Session = Depends(get_db)):
-    try:
-        email = new_user.email
-        does_email_exists = db.exec(select(Registeration)).all()
-        if does_email_exists:
-            print(f"User with the email already exists.")
-            raise HTTPException(status_code= 400, detail="User with the email already exists.")
-        
-        username = new_user.username
-        does_username_exists = db.exec(select(Registeration)).all()
+    email = new_user.email
+    does_email_exists = db.exec(select(Registeration).where(Registeration.email == email)).first()
+    if does_email_exists:
+        print(f"User with the email already exists.")
+        raise HTTPException(status_code= 400, detail="User with the email already exists.")
+    else:
+        print("Email is available")
 
-        if does_username_exists:
-            print("Username already taken.")
-            raise HTTPException(status_code=400, detail="Username already taken.")
-    except:
-        db.add(new_user)
+    username = new_user.username
+    does_username_exists = db.exec(select(Registeration).where(Registeration.username == username)).first()
+
+    if does_username_exists:
+        print("Username already taken.")
+        raise HTTPException(status_code=400, detail="Username already taken.")
+    else:
+        print("Username is available")
+    password = new_user.password
+    hashing_the_password =  bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+    new_user.password = hashing_the_password
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    print(new_user)
+    return {
+        "response": "Registration successfull."
+    }
