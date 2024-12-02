42411927a37dd98955975961f3be47b68a78f99e
Anthony <anthonychinaemeru84@yahoo.com>
2024-12-02 01:07:27 +0100

    Feature: Add get post by iD endpoint

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends, status
-from sqlmodel import Session
+from sqlmodel import Session, select
 from utils.database import get_db
 from schema.schema import PostSchema, PostResponse
 from models.models import Posts
@@ -14,3 +14,12 @@ def create_post(schema: PostSchema, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_posts)
     return new_posts
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=PostResponse)
+def get_post_by_iD(id: int, db: Session = Depends(get_db)):
+    post = db.exec(select(Posts).where(Posts.id == id)).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_200_OK, detail=f"Post with the id '{id}' not found")
+    
+    return post
