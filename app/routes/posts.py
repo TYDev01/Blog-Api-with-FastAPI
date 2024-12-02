556bd215f190b7c92e28e56639aa8e22089a77cd
Anthony <anthonychinaemeru84@yahoo.com>
2024-12-02 01:22:15 +0100

    Feature: Add get post by category endpoint

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from sqlmodel import Session, select
+from typing import List
 from utils.database import get_db
 from schema.schema import PostSchema, PostResponse
 from models.models import Posts
@@ -15,11 +16,20 @@ def create_post(schema: PostSchema, db: Session = Depends(get_db)):
     db.refresh(new_posts)
     return new_posts
 
+# Get post by category
+@router.get("/{category}", status_code=status.HTTP_200_OK, response_model=List[PostResponse])
+def get_post_by_category(category: str, db: Session = Depends(get_db)):
+    post = db.exec(select(Posts).where(Posts.category == category)).all()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Posts under the category '{category}' not found")
+    
+    return post
 
+# Get post by iD
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=PostResponse)
 def get_post_by_iD(id: int, db: Session = Depends(get_db)):
     post = db.exec(select(Posts).where(Posts.id == id)).first()
     if not post:
-        raise HTTPException(status_code=status.HTTP_200_OK, detail=f"Post with the id '{id}' not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with the id '{id}' not found")
     
     return post
